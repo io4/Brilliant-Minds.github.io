95ab043c342f5ae16d7b83333974e82ee879cbf6
nucular <nucular@users.noreply.github.com>
2014-03-28 23:56:18 +0100

    Start with the member list

--- members.js
+++ members.js
@@ -1,3 +1,33 @@
-var USERS = {
-    "KydonShadow": "1519"
-};
+function load() {
+    var s = document.location.search.substring(1);
+    if (s == "")
+        createList();
+    else
+        createRecord(s);
+}
+
+function createList() {
+    var resp = jQuery.getJSON("members.json");
+    resp.done(function(e) {
+        //
+    });
+}
+
+function createRecord(name) {
+    var resp = jQuery.getJSON("members/" + name + ".json");
+    resp.done(function(e) {
+        $("<a href=\"members.html\">< Back</a>").appendTo("#content");
+        title = $("<b>" + name + " </h4>");
+        $("<span class=\"badge\">" + e.rank + "</span>").appendTo(title);
+        title.prependTo("#content");
+    });
+
+    resp.error(function(e) {
+        // Prepend an error message
+        $("<a href=\"members.html\">< Back</a>").appendTo("#content");
+        $("<div class=\"alert alert-error\">Could find no member named \""
+            + name + "\" :(</div>").appendTo("#content");
+    });
+}
+
+$(load);
