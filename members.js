8a4d20a38962f337edbbc22cef77dc48858ec29e
wolfy1339 <wolfy1339@outlook.com>
2014-06-22 14:23:52 -0400

    Add probationary to number of members

--- members.js
+++ members.js
@@ -116,7 +116,7 @@ function createList() {
 
         // Add the date of the last update and a count of active members
         $("<small class=\"text-muted\">Active members: " + 
-            (e.officers.length + e.preofficers.length) + "</small><br\>").appendTo(preoffbox)
+            (e.officers.length + e.preofficers.length + e.probationary.length) + "</small><br\>").appendTo(preoffbox)
         $("<small class=\"text-muted\">Last updated: " + e.updated + "</small>").appendTo(preoffbox);
 
         preoffbox.appendTo("#content");
