io4/Brilliant-Minds.github.io

commit 03c3f2130e20fda0da49da62ed1fa0ad527d9e34

Update divisions list

divisions.js
@@ -57,7 +57,18 @@ function createList() {
         bmntd.appendTo(technicalbox);
         technicalbox.appendTo("#content");
 
-        $("<h4>BMN Records Division (" + e.bmnrd.length + ")</h4>").appendTo(securitybox);
+        $("<h4>BMN Public Relations Division (" + e.bmnprd.length + ")</h4>").appendTo(securitybox);
+        var bmnprd = $("<ul></ul>");
+        for (var i = 0; i < e.bmnprd.length; i++) {
+            var li = $("<li></li>");
+            $("<a href=\"members.html?" + encodeURIComponent(e.bmnprd[i][0]) + "\">" +
+                e.bmnprd[i][0] + "</a><span> </span>").appendTo(li);
+            createRankBadge(e.bmnprd[i][1]).appendTo(li);
+            li.appendTo(bmnprd);
+        }
+        bmnprd.appendTo(securitybox);
+
+        $("<h4>BMN Records Division (" + e.bmnrd.length + ")</h4>").appendTo(technicalbox);
         var bmnrd = $("<ul></ul>");
         for (var i = 0; i < e.bmnrd.length; i++) {
             var li = $("<li></li>");
@@ -66,8 +77,19 @@ function createList() {
             createRankBadge(e.bmnrd[i][1]).appendTo(li);
             li.appendTo(bmnrd);
         }
-        bmnrd.appendTo(securitybox);
+        bmnrd.appendTo(technicalbox);
 
+
+        $("<h4>BMN Research and Development Division (" + e.bmnrdd.length + ")</h4>").appendTo(securitybox);
+        var bmnrdd = $("<ul></ul>");
+        for (var i = 0; i < e.bmnrdd.length; i++) {
+            var li = $("<li></li>");
+            $("<a href=\"members.html?" + encodeURIComponent(e.bmnrdd[i][0]) + "\">" +
+                e.bmnrdd[i][0] + "</a><span> </span>").appendTo(li);
+            createRankBadge(e.bmnrdd[i][1]).appendTo(li);
+            li.appendTo(bmnrdd);
+        }
+        bmnrd.appendTo(securitybox);
         // Add the date of the last update
         $("<small class=\"text-muted\">Last updated: " + e.updated + "</small>").appendTo(technicalbox);
 

commit 1224f45906e5bbea0a6ad003b581caaf2695c85e

Such error on my part this commit fixes a bug introduced in 03c3f2130e20fda0da49da62ed1fa0ad527d9e34

divisions.js
@@ -89,7 +89,7 @@ function createList() {
             createRankBadge(e.bmnrdd[i][1]).appendTo(li);
             li.appendTo(bmnrdd);
         }
-        bmnrd.appendTo(securitybox);
+        bmnrdd.appendTo(securitybox);
         // Add the date of the last update
         $("<small class=\"text-muted\">Last updated: " + e.updated + "</small>").appendTo(technicalbox);
 